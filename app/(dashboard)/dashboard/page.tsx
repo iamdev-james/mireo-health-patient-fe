b21b74f2268370b2978b9a9b17c72bc4c806598a
Adigun James <jamesoluwabukola87@gmail.com>
2026-01-02 08:15:09 +0100

    fix: lint fixes

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,7 @@
 // app/(dashboard)/dashboard/page.tsx
 
 const DashboardPage = () => {
-  return (
-    <div>DashboardPage</div>
-  )
+  return <div>DashboardPage</div>
 }
 
 export default DashboardPage
